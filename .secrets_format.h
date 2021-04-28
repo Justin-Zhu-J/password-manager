337feb4a4515c58521f2e7c35d6753f5fbeb25c4
Justin Zhu <justinzhujz@gmail.com>
2021-04-28 15:22:33 -0500

    Include .secrets_format.h

---
+++ .secrets_format.h
@@ -0,0 +1,13 @@
+#include <string.h>
+using namespace std;
+
+const string key_hash = "HASH_GOES_HERE";
+
+const string PASSWORD_FILE = "PASSWORD_FILE";
+const string ENC_PASSWORD_FILE = "ENC_PASSWORD_FILE";
+
+const string KEY_FILE = "KEY_FILE";
+const string ENC_KEY_FILE = "ENC_KEY_FILE";
+
+const string PUBLIC_KEY = "PUBLIC_KEY";
+const string PRIVATE_KEY = "PRIVATE_KEY(PUT_ON_SEPARATE_DRIVE)";
